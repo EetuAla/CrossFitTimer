81973d06cc3460d84aef034e8e24d37f1e8b6633
Eetu Alanen <ezka.alanen@gmail.com>
2024-05-08 15:42:55 +0300

    Timer made for AMRAP page.

--- components/DifferentTimers/Amrap.js
+++ components/DifferentTimers/Amrap.js
@@ -1,11 +1,140 @@
-import { View, Text, Button, StyleSheet } from "react-native";
+import React, { Component } from "react";
+import { Picker } from "@react-native-picker/picker";
+import {
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+  Platform,
+  Alert,
+} from "react-native";
 
-export default function Amrap() {
-  return (
-    <View style={styles.container}>
-      <Text>AMRAP TIMER!</Text>
+const formatNumber = (number) => `0${number}`.slice(-2);
+
+const getRemaining = (time) => {
+  const minutes = Math.floor(time / 60);
+  const seconds = time - minutes * 60;
+  return { minutes: formatNumber(minutes), seconds: formatNumber(seconds) };
+};
+
+const createArray = (length) => {
+  const arr = [];
+  let i = 0;
+  while (i < length) {
+    arr.push(i.toString());
+    i += 1;
+  }
+  return arr;
+};
+
+const timerMinutes = createArray(61);
+const timerSeconds = createArray(60);
+
+export default class Amrap extends Component {
+  state = {
+    remainingSeconds: 0,
+    isRunning: false,
+    selectedMinutes: "0",
+    selectedSeconds: "0",
+  };
+
+  interval = null;
+
+  componentDidUpdate = (prevProp, prevState) => {
+    if (this.state.remainingSeconds === 0 && prevState.remainingSeconds !== 0) {
+      this.stop();
+      Alert.alert("Workout completed! Great job!");
+    }
+  };
+
+  componentWillUnmount() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  start = () => {
+    this.setState((state) => ({
+      remainingSeconds:
+        parseInt(state.selectedMinutes, 10) * 60 +
+        parseInt(state.selectedSeconds, 10),
+      isRunning: true,
+    }));
+    this.interval = setInterval(() => {
+      this.setState((state) => ({
+        remainingSeconds: state.remainingSeconds - 1,
+      }));
+    }, 1000);
+  };
+
+  stop = () => {
+    clearInterval(this.interval);
+    this.interval = null;
+    this.setState({
+      remainingSeconds: 5,
+      isRunning: false,
+    });
+  };
+
+  renderPickers = () => (
+    <View style={styles.pickerContainer}>
+      <Picker
+        style={styles.picker}
+        itemStyle={styles.pickerItem}
+        selectedValue={this.state.selectedMinutes}
+        onValueChange={(itemValue) => {
+          this.setState({ selectedMinutes: itemValue });
+        }}
+        mode="dropDown"
+      >
+        {timerMinutes.map((value) => (
+          <Picker.Item key={value} label={value} value={value} />
+        ))}
+      </Picker>
+      <Text style={styles.pickerItem}>minutes</Text>
+      <Picker
+        style={styles.picker}
+        itemStyle={styles.pickerItem}
+        selectedValue={this.state.selectedSeconds}
+        onValueChange={(itemValue) => {
+          this.setState({ selectedSeconds: itemValue });
+        }}
+        mode="dropDown"
+      >
+        {timerSeconds.map((value) => (
+          <Picker.Item key={value} label={value} value={value} />
+        ))}
+      </Picker>
+      <Text style={styles.pickerItem}>seconds</Text>
     </View>
   );
+
+  render() {
+    const { minutes, seconds } = getRemaining(this.state.remainingSeconds);
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        {this.state.isRunning ? (
+          <Text style={styles.timerText}>{`${minutes}:${seconds}`}</Text>
+        ) : (
+          this.renderPickers()
+        )}
+        {this.state.isRunning ? (
+          <TouchableOpacity
+            onPress={this.stop}
+            style={[styles.button, styles.buttonStop]}
+          >
+            <Text style={[styles.buttonText, styles.buttonTextStop]}>Stop</Text>
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity onPress={this.start} style={styles.button}>
+            <Text style={styles.buttonText}>Start</Text>
+          </TouchableOpacity>
+        )}
+      </View>
+    );
+  }
 }
 const styles = StyleSheet.create({
   container: {
@@ -13,5 +142,50 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {
+    borderWidth: 10,
+    borderColor: "#89AAFF",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
+  },
+  buttonStop: {
+    borderColor: "#FF851B",
+  },
+  buttonText: {
+    fontSize: 45,
+    color: "#89AAFF",
+  },
+  buttonTextStop: {
+    color: "#FF851B",
+  },
+  timerText: {
+    color: "#000000",
+    fontSize: 90,
+  },
+  picker: {
+    flex: 1,
+    maxWidth: 100,
+    ...Platform.select({
+      android: {
+        color: "#000000",
+        backgroundColor: "rgba(92, 92, 92, 0.206)",
+      },
+    }),
+  },
+  pickerItem: {
+    color: "#000000",
+    fontSize: 20,
+    ...Platform.select({
+      android: {
+        marginLeft: 10,
+        marginRight: 10,
+      },
+    }),
+  },
+  pickerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
